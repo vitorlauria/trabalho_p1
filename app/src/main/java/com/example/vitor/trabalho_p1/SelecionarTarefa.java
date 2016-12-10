d7afca48224e00cbd6fba10d20cbfde7d169f698
Vitor <vitorlauria@gmail.com>
2016-12-10 00:42:29 -0200

    P2 - Alteração 2

--- app/src/main/java/com/example/vitor/trabalho_p1/SelecionarTarefa.java
+++ app/src/main/java/com/example/vitor/trabalho_p1/SelecionarTarefa.java
@@ -10,6 +10,7 @@ public class SelecionarTarefa extends MainActivity {
 
     private Button btnSair;
     private Button btnCadastro;
+    private Button btnProgresso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,16 @@ public class SelecionarTarefa extends MainActivity {
             public void onClick(View v) {
                 Intent chamaCadastroALuno = new Intent (SelecionarTarefa.this, CadastroAluno.class);
                 startActivity(chamaCadastroALuno);
+            }
+        });
+
+        btnProgresso = (Button) findViewById(R.id.btnProgresso);
+
+        btnProgresso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chamaProgressoAula = new Intent (SelecionarTarefa.this, ProgressoAula.class);
+                startActivity(chamaProgressoAula);
             }
         });
     }
