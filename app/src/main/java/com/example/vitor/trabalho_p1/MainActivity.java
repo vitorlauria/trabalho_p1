4bd6bea62238de3b9c838ecbcdab21cc85334473
Vitor <vitorlauria@gmail.com>
2016-12-10 00:00:45 -0200

    P2

--- app/src/main/java/com/example/vitor/trabalho_p1/MainActivity.java
+++ app/src/main/java/com/example/vitor/trabalho_p1/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.vitor.trabalho_p1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAcessar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAcessar = (Button) findViewById(R.id.btnAcessar);
+
+        btnAcessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent chamaSelecionarTarefa = new Intent(MainActivity.this, SelecionarTarefa.class);
+                startActivity(chamaSelecionarTarefa);
+
+            }
+        });
+
+
+
     }
+
+
 }
--- app/src/main/java/com/example/vitor/trabalho_p1/SelecionarTarefa.java
+++ app/src/main/java/com/example/vitor/trabalho_p1/SelecionarTarefa.java
@@ -1,13 +1,28 @@
 package com.example.vitor.trabalho_p1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SelecionarTarefa extends AppCompatActivity {
+public class SelecionarTarefa extends MainActivity {
+
+    private Button btnSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selecionar_tarefa);
+
+        btnSair = (Button) findViewById(R.id.btnSair);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent retornaMainActivity = new Intent (SelecionarTarefa.this, MainActivity.class);
+                startActivity(retornaMainActivity);
+            }
+        });
     }
 }
