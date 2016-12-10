04058465345dc52b18e82c583353b0e2d9f347a5
Vitor <vitorlauria@gmail.com>
2016-12-10 18:26:32 -0200

    P2 - Alteração 4

--- app/src/main/java/com/example/vitor/trabalho_p1/CadastroAluno.java
+++ app/src/main/java/com/example/vitor/trabalho_p1/CadastroAluno.java
@@ -1,14 +1,17 @@
 package com.example.vitor.trabalho_p1;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class CadastroAluno extends SelecionarTarefa {
 
     private Button btnVoltar;
+    private TextView nmAluno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +28,14 @@ public class CadastroAluno extends SelecionarTarefa {
                 startActivity(retornaSelecionarTarefa);
             }
         });
+
+    }
+
+    public void cadastrarAluno(View view) {
+        AlertDialog alertDialog;
+        alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Cadastrado");
+        alertDialog.setMessage("Aluno cadastrado com sucesso!");
+        alertDialog.show();
     }
 }
