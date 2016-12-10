fa2977c0c88dc18ffe42f43329bbc9bb2f165f7d
Vitor <vitorlauria@gmail.com>
2016-12-10 01:35:23 -0200

    P2 - Alteração 2

--- app/src/main/java/com/example/vitor/trabalho_p1/ProgressoAula.java
+++ app/src/main/java/com/example/vitor/trabalho_p1/ProgressoAula.java
@@ -1,13 +1,28 @@
 package com.example.vitor.trabalho_p1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProgressoAula extends AppCompatActivity {
+
+    private Button btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progresso_aula);
+
+        btnVoltar = (Button) findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent retornaSelecionarTarefa = new Intent (ProgressoAula.this, SelecionarTarefa.class);
+                startActivity(retornaSelecionarTarefa);
+            }
+        });
     }
 }
